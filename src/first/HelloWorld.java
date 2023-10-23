8fb2df02aa9949a52e881ba272702201e8e7cc93
Corb G. Lois-Miruna <cti22a207@cst.uoradea.local>
2023-10-23 18:47:24 +0300

    Initial commit

---
+++ src/first/HelloWorld.java
@@ -0,0 +1,9 @@
+package first;
+
+import javax.swing.*;
+
+public class HelloWorld {
+        public static void main(String[] args) {
+            JOptionPane.showMessageDialog(null, "Hello, World!");
+        }
+}
